eeffb20557bcb2694a0e0dcf6793d5784f59f31e
BlackThunder24 <jaimegarcia9999c@gmail.com>
2021-07-22 15:24:11 +0000

    Hastables

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,28 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - creates a hash table.
+ * @size: the size of the array
+ *
+ * Return: pointer to the newly created hash table
+ * If something went wrong, your function should return NULL
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+  hash_table_t *new;
+
+  if (size == 0)
+    return (NULL);
+
+  new = malloc(sizeof(hash_table_t));
+  if (new == NULL)
+    return (NULL);
+
+  new->array = malloc(sizeof(hash_node_t *) * size);
+  if (new == NULL)
+    return (NULL);
+
+  new->size = size;
+
+  return (new);
+}
