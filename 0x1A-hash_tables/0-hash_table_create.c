e970a8e2d92e7768e5ec890f43992fe88a05e6f1
Jaime garcia <62820685+BlackThunder24@users.noreply.github.com>
2021-07-22 10:29:15 -0500

    betty

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,20 +9,20 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-  hash_table_t *new;
+hash_table_t *new;
 
-  if (size == 0)
-    return (NULL);
+if (size == 0)
+return (NULL);
 
-  new = malloc(sizeof(hash_table_t));
-  if (new == NULL)
-    return (NULL);
+new = malloc(sizeof(hash_table_t));
+if (new == NULL)
+return (NULL);
 
-  new->array = malloc(sizeof(hash_node_t *) * size);
-  if (new == NULL)
-    return (NULL);
+new->array = malloc(sizeof(hash_node_t *) * size);
+if (new == NULL)
+return (NULL);
 
-  new->size = size;
+new->size = size;
 
-  return (new);
+return (new);
 }
